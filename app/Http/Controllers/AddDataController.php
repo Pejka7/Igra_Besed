41ff98b848ecd8932f0a483de9f199545f4eb0d9
Pejka7 <mateja12.rojko@gmail.com>
2017-08-07 13:13:02 +0200

    funkcija za izpis xml podatkov za samostalnike in pridevnike na brskalniku

--- app/Http/Controllers/AddDataController.php
+++ app/Http/Controllers/AddDataController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use App\Adjective;
-use Illuminate\Support\Facades\Validator;
+use \XMLReader;
+
 use App\Http\Controllers\Controller;
  
 class AddDataController extends Controller
@@ -19,25 +21,41 @@ class AddDataController extends Controller
     {
        //print_r($request->file('files'));
         $customMessages = ['is_xml_file' => 'Datoteka mora biti tipa xml. Nepravilne datoteke so zapisane v storage/app/file.txt'];
-        
-        $this->validate($request, [
-	    'files' => 'is_xml_file',
-	], $customMessages);
+        $rules = ['files' => 'is_xml_file'];
+        $this->validate($request, $rules, $customMessages);
      
 	//$product = Product::create($request->all());
-        foreach ($request->files as $file) {
-            $filename = $file->store('files');
-           // ProductsPhoto::create([
-           //     'product_id' => $product->id,
-           //     'filename' => $filename
-          //  ]);
+
+        //$request->file('files')->storeAs('files', $request->file('files')->getClientOriginalName());
+        //return $request->file('files');
+       // echo "</br>********************";
+        $xmls = $request->file('files');
+        foreach ($xmls as $xml) {
+            $originalName = $xml->getClientOriginalName();
+            $xml->storeAs('datoteke_xml', $originalName);
+            $filename = storage_path('app\datoteke_xml\\' .$originalName);
+            
+            $xmlReader = new XMLReader();
+            $xmlReader -> open($filename);
+            while($xmlReader->read()){ 
+                if($xmlReader -> nodeType == XMLReader::ELEMENT && $xmlReader -> name == "k"){
+                    $kid = $xmlReader-> getAttribute("kid");
+                    $frek_kol = $xmlReader-> getAttribute("frek_kol");
+                    $jak_kol = $xmlReader -> getAttribute("jak_kol");
+                    $xmlReader -> read();
+                    $noun = $xmlReader -> value;        
+                    print "{$kid}, {$frek_kol}, {$jak_kol}, {$noun}<br>";
+                }
+            }
+            $xmlReader -> close($filename);
+            
         }
+        Storage::deleteDirectory('datoteke_xml');
+        
         
         return 'Upload successful!';
         
-        /*$filename = 'C:\xampp\htdocs\igrabesed_start\storage\app\bel-p.xml';
-        $xmlReader = new XMLReader();
-        $xmlReader -> open($filename);
+        /*
         
         while($xmlReader->read()){ 
             if($xmlReader-> nodeType == XMLReader::ELEMENT && $xmlReader -> name == "k"){
